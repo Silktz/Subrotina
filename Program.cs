cc30d39d0998e66bece74b1c3e1d060a197d1e74
Silktz <caua.franco3@etec.sp.gov.br>
2023-05-12 22:28:15 -0300

    atividadeAULA

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Made by Silktz 
+
+DizerOi(nomeUSER);
+Saudar(nomeUSER); 
+Console.WriteLine("HAHAHA");
+DizerTchau(nomeUSER);
+
+void DizerOi(string nomeUSER)
+{
+    Console.WriteLine($"Oi {nomeUSER}!");
+}
+
+void Saudar(string nomeUSER)
+{
+    Console.WriteLine($"Booom Diaaa {nomeUSER}!");
+}
+
+void DizerTchau(string nomeUSER)
+{
+    Console.Write($"Tchau! {nomeUSER}!");
+    }
+
+Console.WriteLine("");
+int n1 = 10;
+int n2 = 5;
+
+int subtracao1,subtracao2;
+
+subtracao1 = n1 - n2;
+subtracao2 = n2 - n1; 
+ExibirResultado(n1, n2, subtracao1);
+ExibirResultado(n2, n1, subtracao2);
+
+void ExibirResultado(int minuendo, int subtraeno, int diferenca)
+{
+    Console.WriteLine($"{minuendo} - {subtraeno} = {diferenca}");
+}
